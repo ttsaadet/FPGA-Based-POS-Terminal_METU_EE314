17b19725d0386d1901198445f23cb05c21c0967e
Umut Tatlıcı <79724295+utatlici@users.noreply.github.com>
2021-06-19 12:04:09 +0300

    Update horizontal_counter.v

--- horizontal_counter.v
+++ horizontal_counter.v
@@ -8,15 +8,15 @@ output reg [15:0] h_counter = 0;
 
 always @(posedge clk_25mhz)
 begin
-  if(h_counter  < 799) 
-    begin
-      h_counter <= h_counter +1;
-      v_counter_trigger <=0;
-    end
-  else 
-    begin
-      h_counter <=0;
-      v_counter_trigger <=1;
-    end
+	if(h_counter  < 799) 
+		begin
+			h_counter <= h_counter + 1;
+			v_counter_trigger <= 0;
+		end
+	else 
+		begin
+			h_counter <= 0;
+			v_counter_trigger <= 1;
+		end
 end
 endmodule
