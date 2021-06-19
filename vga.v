4d573413b0d4980ae1294557b1123ef226ca7dfa
ttsaadet <52102960+ttsaadet@users.noreply.github.com>
2021-06-19 15:38:02 +0300

    8bit rgb added

--- vga.v
+++ vga.v
@@ -4,9 +4,9 @@ module vga(clk, HSync, VSync, Red, Green, Blue);
 input clk;
 output HSync;
 output VSync;
-output [3:0] Red;
-output [3:0] Green;
-output [3:0] Blue;
+output [7:0] Red;
+output [7:0] Green;
+output [7:0] Blue;
 
 reg clk_25mhz;
 wire v_counter_trigger;
@@ -18,13 +18,13 @@ always @(posedge clk)begin
 end
 
 horizontal_counter h_vga(clk_25mhz, v_counter_trigger, h_counter);
-horizontal_counter v_vga(clk_25mhz, v_counter_trigger, v_counter);
+vertical_counter v_vga(clk_25mhz, v_counter_trigger, v_counter);
 
 assign HSync = (H_counter < 96) ? 1'b1:1'b0;
 assign VSync = (V_counter < 2) ? 1'b1:1'b0;
 
-assign Red = (H_counter < 784 && H_counter > 143 && V_counter < 515 &&  V_counter > 34) ? 4'hF:4'h0;
-assign Green = (H_counter < 784 && H_counter > 143 && V_counter < 515 &&  V_counter > 34) ? 4'hF:4'h0;
-assign Blue = (H_counter < 784 && H_counter > 143 && V_counter < 515 &&  V_counter > 34) ? 4'hF:4'h0;
+assign Red = (H_counter < 784 && H_counter > 143 && V_counter < 515 &&  V_counter > 34) ? 8'hF_F:8'h00;
+assign Green = (H_counter < 784 && H_counter > 143 && V_counter < 515 &&  V_counter > 34) ? 8'hF_F:8'h00;
+assign Blue = (H_counter < 784 && H_counter > 143 && V_counter < 515 &&  V_counter > 34) ? 8'hF_F:8'h00;
 
 endmodule
