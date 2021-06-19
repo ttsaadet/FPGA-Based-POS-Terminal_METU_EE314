724d6d7e82e60dfc945766c467b20edb16e2ca8b
Umut Tatlıcı <79724295+utatlici@users.noreply.github.com>
2021-06-19 12:05:39 +0300

    Update vertical_counter.v

--- vertical_counter.v
+++ vertical_counter.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+
 module vertical_counter(clk_25mhz, v_counter_trigger, v_counter);
 
 input clk_25mhz;
@@ -6,13 +7,14 @@ input v_counter_trigger;
 output reg [15:0] v_counter = 0;
 
 always @(posedge clk_25mhz)
+begin
+  if(v_counter_trigger == 1'b1) 
   begin
-	if(v_counter_trigger == 1'b1) begin
-		 if(v_counter < 524)
-			  v_counter <= v_counter + 1;
-		 else
-			v_counter = 0;
-	end
+    if(v_counter < 524)
+      v_counter <= v_counter + 1;
+    else
+      v_counter = 0;
+  end
 end
 
 endmodule
