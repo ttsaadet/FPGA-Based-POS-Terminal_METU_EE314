a7fdbd68b02f6bb4501714c322a5585b67fbc6aa
ttsaadet <52102960+ttsaadet@users.noreply.github.com>
2021-07-04 16:25:25 +0300

    bug fixes and minor updates

--- buttonManager.sv
+++ buttonManager.sv
@@ -1,21 +1,20 @@
-`timescale 1 ns / 1 ps
+`timescale 1 ns / 1 ns
 module buttonManager(
 	input wire clk,
 	input wire [3:0] button_nonFitered,
 	input wire [3:0] switches,
-	output reg sold_flag,
 	output reg [3:0] soldItemCount,             //kaç çeşit item satıldığının sayısı
+	output reg [3:0] h_rowIndex,//highlighted row in list navigation mode
+	output reg [2:0] barcode[4:0],	// first 4 digit barcode + last 1 digit quantity
 	output reg [2:0] quantitylist[0:11],  //tüm meyvelerin kaç tane alındığının listesi
 	output reg [3:0] shopping_list[0:11], //shopping listi oluşturmak için kronolojik alım listesi
 	output reg [9:0] highligthPosX[0:11], 
 	output reg [9:0] highligthPosY[0:11]
 );
 
-
+reg sold_flag;
 reg[2:0] index; //barcode digit index
 reg clk_down;   //button requires too slow clock ~10hz
-integer counter;
-reg [2:0] barcode[4:0];	// first 4 digit barcode + last 1 digit quantity
 //fruit id list used for highlighting search
 reg[2:0] fruit_list[0:11][0:3] = '{ 
 '{3,1,2,1},'{3,1,2,4},'{3,1,3,3},
@@ -26,7 +25,7 @@ reg[2:0] fruit_list[0:11][0:3] = '{
 reg[9:0] fruit_posX[0:11] = '{280,400,520,280,400,520,280,400,520,280,400,520};
 reg[9:0] fruit_posY[0:11] = '{5,5,5,125,125,125,245,245,245,365,365,365};
 
-reg [3:0] rowIndex = 0;
+
 reg [3:0] fruitIndex = 0;
 reg [2:0] quantity = 0;
 reg [2:0] soldItem[3:0];
@@ -39,10 +38,11 @@ initial begin
 	barcode <= '{0,0,0,0,0};
 	shopping_list[0:11] =  '{15,15,15, 15,15,15, 15,15,15, 15,15,15}; //sort according to which item sold first //15: blank
 	quantitylist[0:11] = '{0,0,0, 0,0,0, 0,0,0, 0,0,0};
-	soldItemCount <= 0;;
+	soldItemCount <= 0;
+	h_rowIndex = 0;
 end
 reg [3:0] buttons;
-
+integer  counter;
 always @(posedge clk)begin
 	if(counter < 10) //change it according to desired frequncy 
 	counter <= counter + 1;
@@ -55,6 +55,7 @@ buttonFilter b1(clk_down,button_nonFitered[0],buttons[0]);
 buttonFilter b2(clk_down,button_nonFitered[1],buttons[1]);
 buttonFilter b3(clk_down,button_nonFitered[2],buttons[2]);
 buttonFilter b4(clk_down,button_nonFitered[3],buttons[3]);
+
 always @(posedge clk_down)begin
 	case(switches)
 		4'b0000:begin  // all switches off, barcode entry mode active
@@ -62,19 +63,19 @@ always @(posedge clk_down)begin
 				case(buttons)
 					4'b0001: begin
 						barcode[index] <= 3'b001;  //1 inserted
-						index <= index + 1;
+						index <= index + 1'b1;
 					end
 					4'b0010:begin
 						barcode[index] <= 3'b010;  //2 inserted
-						index <= index + 1;
+						index <= index + 1'b1;
 					end
 					4'b0100: begin 
 						barcode[index] <= 3'b011;	//3 inserted
-						index <= index+1;
+						index <= index+1'b1;
 					end
 					4'b1000: begin
 						barcode[index] <= 3'b100;	// 4 insterted
-						index <= index+1;
+						index <= index+1'b1;
 					end
 				endcase
 				
@@ -82,7 +83,7 @@ always @(posedge clk_down)begin
 					success_digit_count = 0;
 					for(reg[2:0] digit = 0; digit < 4 ; digit = digit + 1)begin
 						if (fruit_list[fruit_index][digit] == barcode[digit] && digit < index) begin
-							success_digit_count = success_digit_count + 1;
+							success_digit_count = success_digit_count + 1'b1;
 							if(success_digit_count == index) begin
 								highligthPosX[fruit_index] <= fruit_posX[fruit_index];
 								highligthPosY[fruit_index] <= fruit_posY[fruit_index];
@@ -121,17 +122,17 @@ always @(posedge clk_down)begin
 			else quantity <= 0;
 		end
 		4'b0010:begin //sw-2 active, navigation in shopping list active
-			if(buttons[3] == 1'b1 &&  rowIndex > 0) //down move
-				rowIndex <= rowIndex + 1;
-			else if(buttons[2] == 1 && rowIndex < soldItemCount) // up move
-				rowIndex <= rowIndex -1 ;
+			if(buttons[3] == 1'b1 &&  h_rowIndex > 0) //down move
+				h_rowIndex <= h_rowIndex + 1;
+			else if(buttons[2] == 1 && h_rowIndex < soldItemCount) // up move
+				h_rowIndex <= h_rowIndex -1 ;
 			else if(buttons[0] == 1 && buttons[1] == 1)begin  //delete item
-				quantitylist[shopping_list[rowIndex]] <= 0;
+				quantitylist[shopping_list[h_rowIndex]] <= 0;
 				soldItemCount <= soldItemCount - 1;
 				for(reg[3:0] i = 0; i < 12; i = i +1)begin
 					if(i == 11)
 					shopping_list[i] <= 15;
-					else if (i >= rowIndex)	
+					else if (i >= h_rowIndex)	
 					shopping_list[i] <= shopping_list[i + 1]; //aşağıdakileri bir yukarı kaydır
 				end
 			end
@@ -140,13 +141,13 @@ always @(posedge clk_down)begin
 		4'b0100:begin //sw-3 active navigation between images active
 			quantity <=0;		
 			if(buttons[0] == 1'b1 &&  fruitIndex < 11) //right move
-				fruitIndex <= fruitIndex + 1;
+				fruitIndex <= fruitIndex + 4'd1;
 			else if(buttons[1] == 1'b1 && fruitIndex > 0)  //left move
-				fruitIndex <= fruitIndex - 1;
+				fruitIndex <= fruitIndex - 4'd1;
 			else if (buttons[2] == 1'b1 && fruitIndex > 2) //up move
-				fruitIndex <= fruitIndex - 3;
+				fruitIndex <= fruitIndex - 4'd3;
 			else if (buttons[3] == 1'b1 && fruitIndex < 9) //down move
-				fruitIndex <= fruitIndex + 3;
+				fruitIndex <= fruitIndex + 4'd3;
 			
 			for(reg[3:0] i = 0 ; i<12 ; i = i + 1)begin
 				if(i == fruitIndex)begin
